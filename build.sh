f0333fe775886f3942ae2d5c116f5c39477d63c8
Andrew Johnston <apjohnsto@gmail.com>
2014-05-31 22:47:13 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+NODE_HOME=/media/storage/AJohnston/Code/node/git/node-v0.10.1
+XML_HOME=/usr/include/libxml2
 rm -f lib/processor.node
-g++ -O3 -DNDEBUG -fPIC -DPIC -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I/media/storage/AJohnston/Code/node/git/node-v0.10.1/src -I/media/storage/AJohnston/Code/node/git/node-v0.10.1/deps/uv/include -I/media/storage/AJohnston/Code/node/git/node-v0.10.1/deps/v8/include -I/usr/include/libxml2 src/processor.cpp -c -o processor.o
+g++ -O3 -DNDEBUG -fPIC -DPIC -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$NODE_HOME/src -I$NODE_HOME/deps/uv/include -I$NODE_HOME/deps/v8/include -I$XML_HOME src/processor.cpp -c -o processor.o
 g++ processor.o -o lib/processor.node -lxml2 -lxslt -shared -L/usr/local/lib
 rm -f *.o
